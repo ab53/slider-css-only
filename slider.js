ac1fab551b7cb7cc51b68ccda5407790d0ebeff9
lan3945663 <lan3945663@gmail.com>
2021-10-16 22:51:58 +0300

    feat: change slide and text using dots (Sat, Oct, 16, 2021 10:51:58 PM)

--- slider.js
+++ slider.js
@@ -1,32 +1,32 @@
 const text = {
-  0: 'a little disappointed',
+  0: 'a little upset',
   1: 'waiting for good',
   2: 'very surprised',
   3: 'angry',
 }
 
-let right = document.querySelector('.right')
-let left = document.querySelector('.left')
 let carousel = document.querySelector('.carousel')
+let slideText = document.querySelector('.carousel-text')
+let dots = document.querySelectorAll('.carousel-control');
+let slideIndex = 0;
 
-let items = document.querySelectorAll('.carousel-item')
+slideText.textContent = text[slideIndex];
 
-let currentSlide = 0
+dots.forEach(dot => {
+  dot.addEventListener('click', e => {
 
+    let el;
+    if(e.target.classList.contains('carousel-control')) {
+      el = e.target;
+    } else {
+      el = e.target.closest('.carousel-control');
+    }
 
-
-right.addEventListener('click', (e) => {
-  console.log(e, carousel.style)
-  currentSlide += 1
-
-  console.log(e, currentSlide)
-  carousel.style.transform = `translateX(${-720 * currentSlide}px)`;
-})
-
-left.addEventListener('click', (e) => {
-  console.log(e, carousel.style)
-  currentSlide -= 1
-
-  console.log(e, currentSlide)
-  carousel.style.transform = `translateX(${-720 * currentSlide}px)`;
+    dots[slideIndex].classList.remove('active');
+    slideIndex = el.dataset.slide;
+    dots[slideIndex].classList.add('active');
+    el.classList.add('active');
+    slideText.textContent = text[slideIndex];
+    carousel.style.transform = `translateX(${-720 * slideIndex}px)`;
+  })
 })
