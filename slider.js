97575ce214832ddd850660df992865c8efffb490
lan3945663 <lan3945663@gmail.com>
2021-10-16 21:50:53 +0300

    feat: add control && some css (Sat, Oct, 16, 2021 09:50:53 PM)

--- slider.js
+++ slider.js
@@ -1,1 +1,32 @@
-console.log('init');
+const text = {
+  0: 'a little disappointed',
+  1: 'waiting for good',
+  2: 'very surprised',
+  3: 'angry',
+}
+
+let right = document.querySelector('.right')
+let left = document.querySelector('.left')
+let carousel = document.querySelector('.carousel')
+
+let items = document.querySelectorAll('.carousel-item')
+
+let currentSlide = 0
+
+
+
+right.addEventListener('click', (e) => {
+  console.log(e, carousel.style)
+  currentSlide += 1
+
+  console.log(e, currentSlide)
+  carousel.style.transform = `translateX(${-720 * currentSlide}px)`;
+})
+
+left.addEventListener('click', (e) => {
+  console.log(e, carousel.style)
+  currentSlide -= 1
+
+  console.log(e, currentSlide)
+  carousel.style.transform = `translateX(${-720 * currentSlide}px)`;
+})
